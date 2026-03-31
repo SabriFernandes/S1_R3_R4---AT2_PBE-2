8f8a2f4c5e7df6473d06b9816b390928d20cebae
SABRINA FERNANDES DRAGONETI <sabrina.dragoneti2@senaisp.edu.br>
2026-03-31 15:20:23 -0300

    Primeiro commit

---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+//Centralizador de rotas
+
+import { Router } from "express";
+const routes = Router(); 
+import categoriaRoutes from "./categoriaRoutes.js";
+import produtoRoutes from "./produtoRoutes.js"
+
+routes.use('/categorias', categoriaRoutes); 
+routes.use('/produtos', produtoRoutes);
+
+export default routes;
